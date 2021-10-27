98bdab35511dda8542cd6fba08d26723280cd534
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2021-10-27 09:21:20 +0700

    fix typo

--- startup.sh
+++ startup.sh
@@ -2,11 +2,11 @@
 
 cp .env.example .env
 
-sed -i 's|${API_PORT)|'$API_PORT'|' .env
-sed -i 's|${COMPANY_CODE_VA)|'$COMPANY_CODE_VA'|' .env
-sed -i 's|${SUB_COMPANY_CODE_VA)|'$SUB_COMPANY_CODE_VA'|' .env
-sed -i 's|${SQL_URI)|'$SQL_URI'|' .env
-sed -i 's|${OAUTH_SERVER_TOKEN_VALIDATION)|'$OAUTH_SERVER_TOKEN_VALIDATION'|' .env
-sed -i 's|${SECRET)|'$SECRET'|' .env
+sed -i 's|${API_PORT}|'$API_PORT'|' .env
+sed -i 's|${COMPANY_CODE_VA}|'$COMPANY_CODE_VA'|' .env
+sed -i 's|${SUB_COMPANY_CODE_VA}|'$SUB_COMPANY_CODE_VA'|' .env
+sed -i 's|${SQL_URI}|'$SQL_URI'|' .env
+sed -i 's|${OAUTH_SERVER_TOKEN_VALIDATION}|'$OAUTH_SERVER_TOKEN_VALIDATION'|' .env
+sed -i 's|${SECRET}|'$SECRET'|' .env
 
 node index.js
