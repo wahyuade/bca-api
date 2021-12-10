c4afe00b505cdb55d20977b2b7d2c626cefb6718
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2021-12-10 16:35:10 +0700

    update time

--- libs/Helper.js
+++ libs/Helper.js
@@ -40,9 +40,9 @@ class _Helper {
         let month = String(date.getMonth() + 1).padStart(2, '0')
         let year = String(date.getFullYear())
 
-        let hour = date.getHours()
-        let minute = date.getMinutes()
-        let second = date.getSeconds()
+        let hour = String(date.getHours()).padStart(2, '0')
+        let minute = String(date.getMinutes()).padStart(2, '0')
+        let second = String(date.getSeconds()).padStart(2, '0')
         return `${day}/${month}/${year} ${hour}:${minute}:${second}`
     }
 }
